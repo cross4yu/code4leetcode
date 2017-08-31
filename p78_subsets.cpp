7a06ba3d756f77dde551ba3bb88e73166aafda16
cross4yu <benyu@liangguangyudeMacBook-Pro.local>
2017-08-31 23:03:20 +0800

    20170831
    Today add two to solve

---
+++ p78_subsets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+        vector<vector<int> > subsets(vector<int>& nums)
+        {
+            sort(nums.begin(), nums.end());
+            vector<vector<int> > res(1, vector<int>());
+            for (int i = 0; i < nums.size(); ++i)
+            {
+                int n = res.size();
+                for (int j = 0; j < n; ++j)
+                {
+                    res.push_back(res[j]);
+                    res.back().push_back(nums[i]);
+                }
+            }
+            return res;
+        }
+};
