99999648606fd1e84e20f9d44f8140fc278f49e8
cross4yu <benyu@liangguangyudeMacBook-Pro.local>
2017-09-08 22:05:15 +0800

    20170908
    Today add three

---
+++ p107_binary_tree_level_order_traversal_2.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ *
+ */
+ class Solution {       
+    public:    
+        void tran(TreeNode* root, vector<vector<int> >&res, int depth)
+        {              
+            if (root == NULL) return;
+            if (res.size() == depth)
+                res.push_back(vector<int>());
+            res[depth].push_back(root->val);                                                            
+            tran(root->left, res, depth+1);
+            tran(root->right, res, depth+1);
+        } 
+        vector<vector<int> > levelOrderBottom(TreeNode* root)
+        {              
+            vector<vector<int> >res;
+            tran(root, res, 0); 
+            return vector<vector<int> >(res.rbegin(),res.rend());
+        }              
+             
+};
